bad3f227950891261e0cece71e0c54b843c8596c
leomozzer <mozzer.ferraz@gmail.com>
2023-07-03 23:05:17 -0300

    Powershell handle policy definition, assignment and initiatives

---
+++ scripts/AzurePolicy.ps1
@@ -0,0 +1,158 @@
+# Parameter help description
+param(
+    # Parameter help description
+    [Parameter(Mandatory)]
+    [string]
+    [ValidateSet("management-group", "subscriptions")]
+    $scopeType,
+    [Parameter()]
+    [string]
+    $scope,
+    [Parameter()]
+    [string]
+    $policyLocation
+)
+
+function CreateDefinition {
+    param(
+        [Parameter()]
+        [string]
+        $policyName,
+        [Parameter()]
+        [string]
+        $policyFile
+    )
+    try {
+        switch ($scopeType) {
+            "management-group" { 
+                New-AzPolicyDefinition -Name $policyName `
+                -Policy $policyFile `
+                -ManagementGroupName $scope
+            }
+            "subscriptions" { 
+                New-AzPolicyDefinition -Name $policyName `
+                -Policy $policyFile `
+                -SubscriptionId $scope
+            }
+            Default {
+                Write-Output "Cant run CreateDefinition to $policyName"
+            }
+        }
+    }
+    catch {
+        Write-Output "Something went wrong"
+    }
+}
+
+function CreateAssignment{
+    param(
+        [Parameter()]
+        [string]
+        [ValidateSet("policy", "initiative")]
+        $type,
+        [Parameter()]
+        [string]
+        $policyName,
+        [Parameter()]
+        [string]
+        $location
+
+    )
+    try {
+        switch ($type) {
+            "policy" { 
+                $policyDefinition = Get-AzPolicyDefinition -Name $policyName
+                New-AzPolicyAssignment -Name $policyName `
+                    -PolicyDefinition $policyDefinition `
+                    -Scope "/subscriptions/$scope" `
+                    -IdentityType 'SystemAssigned' `
+                    -Location $location
+            }
+            "initiative" { 
+                New-AzPolicyDefinition -Name $policyName `
+                -Policy $policyFile `
+                -SubscriptionId $scope
+            }
+            Default {
+                Write-Output "Cant run CreateAssignment to $policyName"
+            }
+        }
+    }
+    catch {
+        Write-Output "Something went wrong"
+    }
+}
+
+#List of policy definitions
+$policyDefinitions = @(
+    [pscustomobject]@{
+        name             = "inherit-rg-tags"
+        skip_remediation = false
+        file_name        = "Inherit Tags from Resource Group"
+        location         = "eastus"
+        category         = "Tags"
+        type             = "policy"
+    },
+    [pscustomobject]@{
+        name             = "diagnostic-settings-key-vaults"
+        file_name        = "Diagnostic Settings Key Vaults"
+        location         = "eastus"
+        category         = "Monitoring"
+        type             = "initiative"
+    },
+    [pscustomobject]@{
+        name             = "diagnostic-settings-azure-functions"
+        file_name        = "Diagnostic Settings Azure Functions"
+        location         = "eastus"
+        category         = "Monitoring"
+        type             = "initiative"
+    }
+)
+
+$initiativeDefinitions = @(
+    [pscustomobject]@{
+        initiative_name         = "configure_diagnostic_initiative"
+        initiative_display_name = "Configure Diagnostic Settings"
+        initiative_category     = "Monitoring"
+        initiative_description  = "Deploys and configures Diagnostice Settings"
+        definitions             = @("diagnostic-settings-key-vaults", "diagnostic-settings-azure-functions")
+        assignment_effect       = "DeployIfNotExists"
+    }
+)
+
+foreach($policy in $policyDefinitions){
+    Write-Output $policy.name
+    Write-Output $policy.category
+    $filePath = "../policies/definition/$($policy.category)/$($policy.file_name).json"
+    Write-Output $filePath
+    #CreateDefinition -policyName $policy.file_name -policyFile $filePath
+    # if($policy.type -eq "policy"){
+    #     CreateAssignment -type $policy.type -policyName $policy.file_name -location $policy.location
+    # }
+}
+if($initiativeDefinitions.Length -gt 0){
+    foreach($initiative in $initiativeDefinitions){
+        Write-Output $initiative
+        $initiativePolicies = @()
+        foreach($definition in $policyDefinitions){
+            if($initiative.definitions -contains $definition.name){
+                $initiativePolicies += [pscustomobject]@{
+                    "policyDefinitionId" = (Get-AzPolicyDefinition -Name $definition.file_name).PolicyDefinitionId
+                }
+            }
+        }
+        Write-Output $initiativePolicies
+        $initiativePolicyFile = "../tmp/$($initiative.initiative_name).json"
+        $initiativePolicies | ConvertTo-Json -depth 100 | Out-File $initiativePolicyFile
+        # $initiativeMetadata = [pscustomobject]@{
+        #     "category" = $initiative.initiative_category
+        # }
+        New-AzPolicySetDefinition -Name "$($initiative.initiative_display_name)" `
+            -PolicyDefinition $initiativePolicyFile `
+            -Description "$($initiative.initiative_description)" `
+            -Metadata '{"category":"<<category>>"}'.Replace('<<category>>', $initiative.initiative_category) `
+            -Parameter "../policies/initiative/$($initiative.initiative_display_name)/parameters.json"
+        #Write-Output $initiative
+        #CreateAssignment -type "initiative" -policyName $policy.file_name -location $policyLocation
+    }
+}
