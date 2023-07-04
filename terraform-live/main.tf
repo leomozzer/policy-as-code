e7dd4a8dc99bf95559e720834c3da8f5c4c8c512
leomozzer <mozzer.ferraz@gmail.com>
2023-07-03 23:04:23 -0300

    Changed folder structure

--- terraform-live/main.tf
+++ terraform-live/main.tf
@@ -35,14 +35,14 @@ module "subscription_definition" {
     for index, definition in var.policy_definitions : definition.name => definition
   }
   policy_name         = each.value.file_name
-  display_name        = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.displayName
-  policy_description  = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.description
-  policy_category     = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.metadata.category
-  policy_version      = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.metadata.version
+  display_name        = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.displayName
+  policy_description  = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.description
+  policy_category     = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.metadata.category
+  policy_version      = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.metadata.version
   management_group_id = data.azurerm_management_group.management_group.id
-  policy_rule         = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.policyRule
-  policy_parameters   = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.parameters
-  policy_metadata     = (jsondecode(file("../policies/${each.value.category}/${each.value.file_name}.json"))).properties.metadata
+  policy_rule         = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.policyRule
+  policy_parameters   = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.parameters
+  policy_metadata     = (jsondecode(file("../policies/definition/${each.value.category}/${each.value.file_name}.json"))).properties.metadata
 }
 
 module "subscription_definition_def_assignment" {
